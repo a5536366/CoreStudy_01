96bcdae68bc3fb2b3c873d2ad8d6d81e152e53f8
luckearth <luckearth@luckearth.cn>
2016-09-09 18:09:10 +0800

    分布式缓存基本使用

---
+++ src/CoreStudy_04/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreStudy_04
+{
+    /// <summary>
+    /// 分布式缓存学习
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //注册编码，控制台才可以输出中文
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "AppSetting.json");
+            //设置配置
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile(path, true)
+                .Build();
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddDistributedRedisCache(options =>
+            {
+                //从配置读取地址
+                options.Configuration = configuration.GetConnectionString("RedisConnection");
+                //实例名称
+                options.InstanceName = "master";
+            }); //注册分布式缓存
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IDistributedCache _distributedCache = serviceProvider.GetService<IDistributedCache>();
+            //字符串缓存，可缓存 json
+            _distributedCache.SetString("CoreTest","CoreTest"+DateTime.Now);
+            //二进制数据缓存
+            _distributedCache.Set("CoreTest_byte", Encoding.UTF8.GetBytes("CoreTest_byte"));
+
+            var s = Encoding.UTF8.GetString(_distributedCache.Get("CoreTest_byte"));
+            string str=_distributedCache.GetString("CoreTest");
+            Console.WriteLine(str);
+            Console.WriteLine(s);
+            Console.Read();
+        }
+    }
+}
